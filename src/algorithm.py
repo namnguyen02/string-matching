9295577fa75f870197342f41ee10ffafee6a2b51
TruongNguyen1305 <trngcr71305@gmail.com>
2023-04-06 21:11:45 +0700

    first commit

---
+++ src/algorithm.py
@@ -0,0 +1,165 @@
+import numpy as np
+import pandas as pd
+
+def affine_gap_measure(str1, str2):
+  str1 = str1.lower()
+  str2 = str2.lower()
+  str1_len = len(str1)
+  str2_len = len(str2)
+
+  matrix = np.zeros((str1_len+1, str2_len+1, 3))
+  co = 1
+  cr = 0.5
+  matrix[0][0] = [0, -co, -co]
+
+  #intial step
+  for i in range(1, str1_len + 1):
+    matrix[i][0][0] = -np.inf
+    matrix[i][0][1] = matrix[i-1][0][1] - cr
+    matrix[i][0][2] = -np.inf
+
+  for i in range(1, str2_len + 1):
+    matrix[0][i][0] = -np.inf
+    matrix[0][i][1] = -np.inf
+    matrix[0][i][2] = matrix[0][i-1][2] - cr
+
+  for i in range(1, str1_len + 1):
+    for j in range(1, str2_len + 1):
+      c = 2 if str1[i-1] == str2[j-1] else -1
+      matrix[i][j][0] = max((matrix[i-1][j-1][0] + c, matrix[i-1][j-1][1] + c, matrix[i-1][j-1][2] + c))
+      matrix[i][j][1] = max((matrix[i-1][j][0] - co, matrix[i-1][j][1] - cr))
+      matrix[i][j][2] = max((matrix[i][j-1][0] - co, matrix[i][j-1][2] - cr))
+
+  return max(matrix[str1_len][str2_len]) / max(str1_len, str2_len)
+
+
+def score(str1, str2):
+    str1_len = len(str1)
+    str2_len = len(str2)
+
+    # calculate edit distance
+    matrix = np.zeros((str1_len + 1, str2_len + 1))
+    matrix[0][0] = 0
+
+    # intial step
+    for i in range(1, str1_len + 1):
+        matrix[i][0] = matrix[i - 1][0] + 1
+
+    for i in range(1, str2_len + 1):
+        matrix[0][i] = matrix[0][i - 1] + 1
+
+    for i in range(1, str1_len + 1):
+        for j in range(1, str2_len + 1):
+            c = 0 if str1[i - 1] == str2[j - 1] else 1
+            matrix[i][j] = min(matrix[i - 1][j - 1] + c, matrix[i - 1][j] + 1, matrix[i][j - 1] + 1)
+
+    return 1 - (matrix[str1_len][str2_len] / max(str1_len, str2_len))
+
+
+def find_max(matrix):
+    if len(matrix) == 0:
+        return 0, 0
+    ele_max, m_max = matrix[0][0], matrix[0][1]
+    for i in range(1, len(matrix)):
+        if matrix[i][0] > ele_max:
+            ele_max = matrix[i][0]
+            m_max = matrix[i][1]
+
+    return ele_max, m_max
+
+
+def generalized_jascard_measure(str1, str2):
+    set1 = list(set([ele.lower().strip() for ele in str1.split(',')]))
+    set2 = list(set([ele.lower().strip() for ele in str2.split(',')]))
+    set1_len = len(set1)
+    set2_len = len(set2)
+    k = 0.5
+    matrix = np.zeros((set1_len, set2_len, 2))
+
+    # initial
+    for i in range(set2_len):
+        point = score(set1[0], set2[i])
+        if point > k:
+            matrix[0][i] = [point, 1]
+        else:
+            matrix[0][i] = [0, 0]
+
+    for i in range(1, set1_len):
+        for j in range(set2_len):
+            point = score(set1[i], set2[j])
+            prev = find_max([matrix[i - 1][t] for t in range(set2_len) if t != j])
+            if point > k:
+                matrix[i][j] = [point + prev[0], prev[1] + 1]
+            else:
+                matrix[i][j] = [0 + prev[0], prev[1]]
+
+    total_score, M = find_max(matrix[set1_len - 1])
+
+    return total_score / (set1_len + set2_len - M)
+
+
+def size_filtering(row, dataset, fields):
+    x = ""
+    for field in fields:
+        x += row[field] + ","
+    x = x.replace(',', ' ').split()
+    max = len(x) / 0.9
+    min = len(x) * 0.9
+    lst = []
+
+    for index, ele in dataset.iterrows():
+        y = ""
+        for field in fields:
+            y += ele[field] + ","
+        y = y.replace(',', ' ').split()
+        if len(y) <= max and len(y) >= min:
+            lst.append(index)
+
+    return dataset.loc[lst]
+
+
+# def string_matching_solve(dataset1, dataset2, field):
+#     mapping_dataset = []
+#     for index1, ele1 in enumerate(dataset1[field].values):
+#         dataset2_after_filter = size_filtering(ele1, dataset2, field)
+#         print(len(dataset2), len(dataset2_after_filter))
+#         for index2, ele2 in enumerate(dataset2_after_filter[field].values):
+#             if generalized_jascard_measure(ele1, ele2) >= 0.8:
+#                 mapping_dataset.append({
+#                     "id1": dataset1.id.values[index1],
+#                     "id2": dataset2_after_filter.id.values[index2]
+#                 })
+#                 print(ele1, '               ', ele2, f'index = {dataset2_after_filter.index.values[index2]}')
+#                 dataset2.drop(index=dataset2_after_filter.index.values[index2], inplace=True)
+#                 break
+#
+#     return pd.DataFrame(mapping_dataset, columns=["id1", "id2"])
+
+def string_matching_solve(dataset1, dataset2, fields):
+    mapping_dataset = []
+    for index1, row1 in dataset1.iterrows():
+        dataset2_after_filter = size_filtering(row1, dataset2, fields)
+        print(len(dataset2), len(dataset2_after_filter))
+        for index2, row2 in dataset2_after_filter.iterrows():
+            for field in fields:
+                avg_point = generalized_jascard_measure(row1[field], row2[field])
+                if(avg_point) < 7:
+                    break
+
+            if avg_point >= 0.7:
+                print(avg_point)
+                mapping_dataset.append({
+                    "id1": dataset1.loc[index1].id,
+                    "id2": dataset2_after_filter.loc[index2].id
+                })
+                print(f'Mapping df2 index = {index2}')
+                dataset2.drop(index=index2, inplace=True)
+                break
+
+    return pd.DataFrame(mapping_dataset, columns=["id1", "id2"])
+
+# df1 = pd.read_csv('../data/cleanCleanErDatasets/DBLP-ACM/ACM.csv', encoding="ISO-8859-1").dropna()
+# df1['year'] = df1['year'].astype(str)
+# df2 = pd.read_csv('../data/cleanCleanErDatasets/DBLP-ACM/DBLP2.csv', encoding="ISO-8859-1").dropna()
+# df2['year'] = df2['year'].astype(str)
+# print(string_matching_solve(df1, df2, ['title', 'authors', 'year']))
---
+++ src/main.py
@@ -0,0 +1,91 @@
+from tkinter import filedialog, messagebox, ttk
+from tkinter import *
+from algorithm import *
+
+#initial
+df1 = None
+df2 = None
+df_mapping = None
+# Create a new window
+root = Tk()
+root.title("Data mining assignment")
+root.geometry("400x400")
+
+def browse_file(filename_input, file_path):
+    filepath = filedialog.askopenfilename()
+
+    # Set the text of the label to the selected file's path
+    filename_input.config(text=filepath.split('/')[-1])
+    file_path.set(filepath)
+
+def solve(fields):
+    fields.remove('year')
+    fields.remove('venue')
+    print(fields)
+
+    global df_mapping
+    df_mapping = string_matching_solve(df1, df2, fields)
+
+def handle_generate_option(path1, path2):
+    if path1.get() == "" or path2.get() == "":
+        messagebox.showerror("Lỗi", "Không tìm thấy file")
+        return
+
+    global df1, df2
+    df1 = pd.read_csv(path1.get(), encoding="ISO-8859-1").dropna()
+    df2 = pd.read_csv(path2.get(), encoding="ISO-8859-1").dropna()
+    common_fields = set(df1.columns.values).intersection(df2.columns.values)
+    common_fields.remove('id')
+    for field in common_fields:
+        if df1[field].dtype != 'O':
+            df1[field] = df1[field].astype(str)
+        if df2[field].dtype != 'O':
+            df2[field] = df2[field].astype(str)
+
+    solve(common_fields)
+    df_mapping.to_csv(f'../result/{filename_input1.cget("text")}_{filename_input2.cget("text")}_mapping.csv', index=False)
+    messagebox.showinfo("Info", "Done !")
+
+
+Label(root, text="String matching problem", font="times 15 bold").grid(row=0, column=3)
+########################################################################
+file_path1 = StringVar()
+
+label_input1 = Label(root, text="Choose The First File")
+label_input1.grid(row=1, column=2)
+
+filename_input1 = Label(root, text="")
+filename_input1.grid(row=2, column=2)
+
+btn1 = Button(text="Browse", command=lambda: browse_file(filename_input1, file_path1))
+btn1.grid(row=1, column=3)
+
+#########################################################################
+file_path2 = StringVar()
+
+label_input2 = Label(root, text="Choose The Second File")
+label_input2.grid(row=3, column=2)
+
+filename_input2 = Label(root, text="")
+filename_input2.grid(row=4, column=2)
+
+btn2 = Button(text="Browse", command=lambda: browse_file(filename_input2, file_path2))
+btn2.grid(row=3, column=3)
+
+########################################################################
+btn_submit = Button(text="Generate mapping file", command=lambda: handle_generate_option(file_path1, file_path2))
+btn_submit.grid(row=5, column=3)
+
+########################################################################
+Label(root, text="").grid(row=6, column=1)
+# Run the window loop
+root.mainloop()
+
+
+
+
+
+
+# y1 = "Gottfried Vossen, Mathias Weske"
+# y2 = "Jan van den Bussche"
+# print(generalized_jascard_measure(y1, y2))
